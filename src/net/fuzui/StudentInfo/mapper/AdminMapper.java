f02be0ea30ccd11cc360d9477435e980e7fdd3a8
E_thic <3514171197@qq.com>
2022-05-18 14:56:37 +0800

    init commit

---
+++ src/net/fuzui/StudentInfo/mapper/AdminMapper.java
@@ -0,0 +1,21 @@
+package net.fuzui.StudentInfo.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @ProjectName: StudentInfo
+ * @Package: net.fuzui.StudentInfo.mapper
+ * @ClassName: AdminMapper
+ * @Description: 管理员数据访问层接口
+ */
+public interface AdminMapper {
+
+    /**
+     *  管理员登录设置
+     * @param aname   管理员账号（唯一）
+     * @param apassword   密码
+     * @return
+     */
+    public String queryByNamePwd(@Param("aname") String aname, @Param("apassword") String apassword);
+
+}
---
+++ src/net/fuzui/StudentInfo/service/AdminService.java
@@ -0,0 +1,22 @@
+package net.fuzui.StudentInfo.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @ProjectName: StudentInfo
+ * @Package: net.fuzui.StudentInfo.service
+ * @ClassName: AdminService
+ * @Description: 管理员service接口
+ */
+
+public interface AdminService {
+
+    /**
+     *  管理员登录设置
+     * @param aname   管理员账号（唯一）
+     * @param apassword   密码
+     * @return
+     */
+    public String queryByNamePwd(String aname, String apassword);
+
+}
